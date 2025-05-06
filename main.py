6a98d35c44a2c039d2f94b3462bdbf015b27bba4
Jaemin <odumag99@gmail.com>
2025-05-06 17:53:33 +0900

    get procedure to wait for the one-pager to be uploaded

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ def main():
     pw = get_playwright()
     browser = pw.chromium.connect_over_cdp(f"http://localhost:{PORT}/", timeout=10000)
     print("지금 열린 페이지에서 인턴하샤 메일함에 들어간 후 Enter를 누르세요.")
-    
     input()
     page = browser.contexts[0].pages[0]
+    page.pause()
 
     # 매크로 실행
     # CSV 읽기
@@ -37,7 +37,7 @@ def main():
         for row in reader:
             recipient_name = row["name"]
             recipient_email = row["email"]
-            print(f"{recipient_name}({recipient_email})에 대한 이메일을 작성하려면 Enter 키를 누르세요.\n")
+            print(f"{recipient_name}({recipient_email})에 대한 이메일을 작성하려면 Enter 키를 누르세요.")
             input()
 
             # 편지쓰기 창 열기
@@ -50,7 +50,7 @@ def main():
 
             # 제목 입력
             page.get_by_role("textbox", name="제목").click()
-            page.get_by_role("textbox", name="제목").fill()
+            page.get_by_role("textbox", name="제목").fill(EMAIL_SUBJECT)
 
             # 본문 입력
             # 본문 내용 생성
@@ -62,7 +62,23 @@ def main():
                 page.get_by_role("button", name="파일 첨부").click()
             file_chooser = fc_info.value
             file_chooser.set_files(os.path.abspath(INTERNHASHA_ONEPAGER_FILE_PATH))
-        
-        page.pause()
+            # 파일 업로드 대기
+            try:
+                progress_bar = page.get_by_role("progressbar", name=f"{os.path.basename(INTERNHASHA_ONEPAGER_FILE_PATH)} 업로드 중")
+                progress_bar.wait_for(state="hidden", timeout=30000)
+            except Exception as e:
+                print("원페이저 업로드 실패")
+                page.pause()
+                raise e
+
+            # 보내기 예약
+            page.get_by_role("button", name="보내기 옵션 더보기").click()
+            page.get_by_text("보내기 예약", exact=True).click()
+            page.get_by_text("날짜 및 시간 선택").click()
+            page.get_by_role("textbox", name="날짜").click()
+            page.get_by_role("textbox", name="날짜").fill(SENDING_DATE)
+            page.get_by_role("textbox", name="시간").click()
+            page.get_by_role("textbox", name="시간").fill(SENDING_TIME)
+            page.get_by_role("button", name="보내기 예약").click()
 
 main()
